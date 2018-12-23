8e778622ead6f999c8a264819c76325629876a7d
Amitabha <amitabhaghosh92@gmail.com>
2018-12-23 22:59:59 +0530

    added shared module to imports array

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -4,6 +4,8 @@ import { ChatBoxComponent } from './chat-box/chat-box.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ToastrModule} from 'ngx-toastr';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { SharedModule } from '../shared/shared.module';
+import { RemoveSpecialCharPipe } from '../shared/pipe/remove-special-char.pipe';
 
 
 
@@ -14,8 +16,9 @@ import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
     ToastrModule.forRoot(),
     RouterModule.forChild([ 
       { path: 'chat', component: ChatBoxComponent }
-    ])
+    ]),
+    SharedModule
   ],
-  declarations: [ChatBoxComponent]
+  declarations: [ChatBoxComponent,RemoveSpecialCharPipe]
 })
 export class ChatModule { }
